796ab5c918914a9d157149493a9a29cc7ab2de90
kyoko0219 <kk0219.business@gmail.com>
2024-09-23 01:22:17 +0900

    new

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-// ルーティングを設定するコントローラーを宣言
-use App\Http\Controllers\PostController;
+
+// ルーティングを設定するコントローラを宣言する
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VendorController;
+use App\Http\Controllers\RequestController;
+use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\SignInController;
+use App\Http\Controllers\CookieController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,14 +28,56 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'Hello, world!';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/posts/create', [PostController::class, 'create']);
+require __DIR__.'/auth.php';
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/hello', [HelloController::class, 'index']);
 
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::get('/products', [ProductController::class, 'index']);
 
-Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::get('/products/create', [ProductController::class, 'create']);
+
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::get('/posts/create', [VendorController::class, 'create'])->middleware('auth');
+
+Route::post('/posts/store', [VendorController::class, 'store'])->name('vendors.store')->middleware('auth');
+
+Route::get('/vendors/{id}', [VendorController::class, 'show']);
+
+Route::get('/requests/create', [RequestController::class, 'create']);
+
+Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
+
+Route::get('/responses', [ResponseController::class, 'index']);
+
+Route::get('/sign-in', [SignInController::class, 'create']);
+
+Route::post('/sign-in', [SignInController::class, 'store'])->name('sign-in.store');
+
+Route::get('/cookies', [CookieController::class, 'index']);
+
+Route::get('/cookies/create', [CookieController::class, 'create'])->name('cookies.create');
+
+Route::post('/cookies/store', [CookieController::class, 'store'])->name('cookies.store');
+
+Route::delete('/cookies/destroy', [CookieController::class, 'destroy'])->name('cookies.destroy');
+
+Route::get('/sessions', [SessionController::class, 'index']);
+
+Route::get('/sessions/create', [SessionController::class, 'create'])->name('sessions.create');
+
+Route::post('/sessions/store', [SessionController::class, 'store'])->name('sessions.store');
+
+Route::delete('/sessions/destroy', [SessionController::class, 'destroy'])->name('sessions.destroy');
